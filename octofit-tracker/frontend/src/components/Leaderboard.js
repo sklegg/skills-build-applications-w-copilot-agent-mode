eb7ffdf2e0a99ccdbb89f58e83641d7daacb1121
Scott Legg <sklegg@gmail.com>
2025-04-10 20:04:02 +0000

    add frontend

---
+++ octofit-tracker/frontend/src/components/Leaderboard.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+
+function Leaderboard() {
+  const [leaderboard, setLeaderboard] = useState([]);
+
+  useEffect(() => {
+    fetch('https://special-orbit-vx4rv9v4jfw5xw-8000.app.github.dev/api/leaderboard')
+      .then(response => response.json())
+      .then(data => setLeaderboard(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Leaderboard</h1>
+      <ul>
+        {leaderboard.map(entry => (
+          <li key={entry.id}>{entry.name}: {entry.score}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Leaderboard;
